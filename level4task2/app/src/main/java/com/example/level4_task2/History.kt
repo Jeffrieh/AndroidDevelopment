76c452563e8b7c94e8d61305e10268e62acafb7f
Jeffrey <jeffrey@1nteger.nl>
2020-03-16 18:29:14 +0100

    small change to clear button

--- level4task2/app/src/main/java/com/example/level4_task2/History.kt
+++ level4task2/app/src/main/java/com/example/level4_task2/History.kt
@@ -7,12 +7,18 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_history.*
+import kotlinx.coroutines.CoroutineScope
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class History : AppCompatActivity() {
 
+    lateinit var db: AppDatabase
 
-    lateinit var db : AppDatabase;
-    lateinit var historyAdapter: Adapter
+    private val items = arrayListOf<HistoryItem>()
+    private val adapter = Adapter(items)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,18 +30,28 @@ class History : AppCompatActivity() {
             AppDatabase::class.java, "HistoryItem.db"
         ).allowMainThreadQueries().build()
 
-        var list = db.HistoryDao().getAll()
-
-        r_history.layoutManager = StaggeredGridLayoutManager(1, LinearLayoutManager.VERTICAL)
-        historyAdapter= Adapter(list)
-        r_history.adapter = historyAdapter
-
-        clear.setOnClickListener({clearHistory()})
+        initViews()
     }
 
-    fun clearHistory(){
+    private fun getItemsFromDatabase() {
+        var itemList = db.HistoryDao().getAll()
+
+        this@History.items.clear()
+        this@History.items.addAll(itemList)
+        this@History.adapter.notifyDataSetChanged()
+    }
+
+    fun initViews() {
+        getItemsFromDatabase()
+
+        clear.setOnClickListener({ clearHistory() })
+        r_history.layoutManager = StaggeredGridLayoutManager(1, LinearLayoutManager.VERTICAL)
+        r_history.adapter = adapter
+
+    }
+
+    fun clearHistory() {
         db.HistoryDao().nukeTable()
-        historyAdapter = Adapter(emptyList())
-        historyAdapter.notifyDataSetChanged()
+        getItemsFromDatabase()
     }
 }
